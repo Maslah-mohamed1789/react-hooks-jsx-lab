3cdb54b3885d1a2394e32c8738663afc0afab29b
maslah <Maslah-mohamed1789>
2024-11-05 15:29:35 +0300

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div>
+    <h2>About Me</h2>
+    <p>The Person Who Never Made a Mistake Never Made Anything</p>
+    <img src={image} alt="I Made This" />
+  </div>;
 }
 
 export default About;
